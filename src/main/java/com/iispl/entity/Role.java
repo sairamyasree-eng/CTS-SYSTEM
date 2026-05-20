be47ecde693cf902d11447d22a741fae551c119e
NANDUALA-iispl <ala.nandu@imageinfosystem.com>
2026-05-20 14:15:42 +0530

    added user and role entity

--- src/main/java/com/iispl/entity/Role.java
+++ src/main/java/com/iispl/entity/Role.java
@@ -1,5 +1,124 @@
 package com.iispl.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.iispl.enums.Status;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+@Entity
 public class Role {
+
+	@Id
+	@Column(name = "id")
+	private String id;
+
+	@Column(name = "roleName")
+	private String roleName;
+
+	@Column(name = "description")
+	private String description;
+
+	@Column(name = "canCreateBatch")
+	private boolean canCreateBatch;
+
+	@Column(name = "canApproveOutward")
+	private boolean canApproveOutward;
+
+	@Column(name = "canApproveInward")
+	private boolean canApproveInward;
+
+	@Enumerated(EnumType.STRING)
+	@Column(name = "status")
+	private Status status;
+
+	@OneToMany(mappedBy = "role", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	private List<User> users = new ArrayList<>();
+
+	public Role() {
+	}
+
+	public Role(String id, String roleName, String description, boolean canCreateBatch, boolean canApproveOutward,
+			boolean canApproveInward, Status status) {
+		this.id = id;
+		this.roleName = roleName;
+		this.description = description;
+		this.canCreateBatch = canCreateBatch;
+		this.canApproveOutward = canApproveOutward;
+		this.canApproveInward = canApproveInward;
+		this.status = status;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getRoleName() {
+		return roleName;
+	}
+
+	public void setRoleName(String roleName) {
+		this.roleName = roleName;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public boolean isCanCreateBatch() {
+		return canCreateBatch;
+	}
+
+	public void setCanCreateBatch(boolean canCreateBatch) {
+		this.canCreateBatch = canCreateBatch;
+	}
+
+	public boolean isCanApproveOutward() {
+		return canApproveOutward;
+	}
+
+	public void setCanApproveOutward(boolean canApproveOutward) {
+		this.canApproveOutward = canApproveOutward;
+	}
+
+	public boolean isCanApproveInward() {
+		return canApproveInward;
+	}
+
+	public void setCanApproveInward(boolean canApproveInward) {
+		this.canApproveInward = canApproveInward;
+	}
+
+	public Status getStatus() {
+		return status;
+	}
+
+	public void setStatus(Status status) {
+		this.status = status;
+	}
+
+	public List<User> getUsers() {
+		return users;
+	}
+
+	public void setUsers(List<User> users) {
+		this.users = users;
+	}
 
 }
--- src/main/java/com/iispl/entity/User.java
+++ src/main/java/com/iispl/entity/User.java
@@ -1,5 +1,137 @@
 package com.iispl.entity;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.iispl.enums.Status;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+@Entity
 public class User {
+
+	@Id
+	@Column(name = "id")
+	private String id;
+
+	@Column(name = "username")
+	private String username;
+
+	@Column(name = "passwordHash")
+	private String passwordHash;
+
+	@Column(name = "fullname")
+	private String fullName;
+
+	@Column(name = "branch")
+	private String branch;
+
+	@Enumerated(EnumType.STRING)
+	@Column(name = "status")
+	private Status status;
+
+	@Column(name = "created_date")
+	private LocalDate createdDate;
+
+	@Column(name = "last_login_at")
+	private LocalDate lastLoginAt;
+
+	@OneToMany(mappedBy = "role", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	private List<User> users = new ArrayList<>();
+
+	private User() {
+	}
+
+	public User(String id, String username, String passwordHash, String fullName, String branch, Status status,
+			LocalDate createdDate, LocalDate lastLoginAt) {
+		this.id = id;
+		this.username = username;
+		this.passwordHash = passwordHash;
+		this.fullName = fullName;
+		this.branch = branch;
+		this.status = status;
+		this.createdDate = createdDate;
+		this.lastLoginAt = lastLoginAt;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPasswordHash() {
+		return passwordHash;
+	}
+
+	public void setPasswordHash(String passwordHash) {
+		this.passwordHash = passwordHash;
+	}
+
+	public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	public String getBranch() {
+		return branch;
+	}
+
+	public void setBranch(String branch) {
+		this.branch = branch;
+	}
+
+	public Status getStatus() {
+		return status;
+	}
+
+	public void setStatus(Status status) {
+		this.status = status;
+	}
+
+	public LocalDate getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(LocalDate createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public LocalDate getLastLoginAt() {
+		return lastLoginAt;
+	}
+
+	public void setLastLoginAt(LocalDate lastLoginAt) {
+		this.lastLoginAt = lastLoginAt;
+	}
+
+	public List<User> getUsers() {
+		return users;
+	}
+
+	public void setUsers(List<User> users) {
+		this.users = users;
+	}
 
 }
