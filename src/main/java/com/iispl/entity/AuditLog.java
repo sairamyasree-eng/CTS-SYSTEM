02fd28c864e0affe4043905bb38b3e03da9bb71e
sai-harshith-0402 <sai.harshith@imageinfosystems.com>
2026-05-20 13:04:19 +0530

    added InwardCheque,AuditLog entities

--- src/main/java/com/iispl/entity/AuditLog.java
+++ src/main/java/com/iispl/entity/AuditLog.java
@@ -1,5 +1,180 @@
 package com.iispl.entity;
 
-public class AuditLog {
+import java.time.LocalDateTime;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="audit_log")
+public class AuditLog {
+	@Id
+	@Column(name="id")
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	@Column(name="user_id")
+	private String userId;
+	@Column(name="session_id")
+	private String sessionId;
+	@Column(name="action")
+	private String action;
+	@Column(name="entity_type")
+	private String entityType;
+	@Column(name="entity_id")
+	private String entityId;
+	@Column(name="old_value")
+	private String odlValue;
+	@Column(name="new_value")
+	private String newValue;
+	@Column(name="ip_address")
+	private String ipAddress;
+	@Column(name="created_at")
+	private LocalDateTime createdAt;
+	
+	
+	
+	public AuditLog() {	}
+
+	public AuditLog(Long id, String userId, String sessionId, String action, String entityType, String entityId,
+			String odlValue, String newValue, String ipAddress, LocalDateTime createdAt, String description) {
+
+		this.id = id;
+		this.userId = userId;
+		this.sessionId = sessionId;
+		this.action = action;
+		this.entityType = entityType;
+		this.entityId = entityId;
+		this.odlValue = odlValue;
+		this.newValue = newValue;
+		this.ipAddress = ipAddress;
+		this.createdAt = createdAt;
+		this.description = description;
+	}
+
+	@Column(name="description")
+	private String description;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name="user_id",nullable=true)
+	private User user;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name="session_id",nullable=true)
+	private CtsSession session;
+
+
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getSessionId() {
+		return sessionId;
+	}
+
+	public void setSessionId(String sessionId) {
+		this.sessionId = sessionId;
+	}
+
+	public String getAction() {
+		return action;
+	}
+
+	public void setAction(String action) {
+		this.action = action;
+	}
+
+	public String getEntityType() {
+		return entityType;
+	}
+
+	public void setEntityType(String entityType) {
+		this.entityType = entityType;
+	}
+
+	public String getEntityId() {
+		return entityId;
+	}
+
+	public void setEntityId(String entityId) {
+		this.entityId = entityId;
+	}
+
+	public String getOdlValue() {
+		return odlValue;
+	}
+
+	public void setOdlValue(String odlValue) {
+		this.odlValue = odlValue;
+	}
+
+	public String getNewValue() {
+		return newValue;
+	}
+
+	public void setNewValue(String newValue) {
+		this.newValue = newValue;
+	}
+
+	public String getIpAddress() {
+		return ipAddress;
+	}
+
+	public void setIpAddress(String ipAddress) {
+		this.ipAddress = ipAddress;
+	}
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public CtsSession getSession() {
+		return session;
+	}
+
+	public void setSession(CtsSession session) {
+		this.session = session;
+	}
+	
+	
+	
 }
--- src/main/java/com/iispl/entity/InwardCheque.java
+++ src/main/java/com/iispl/entity/InwardCheque.java
@@ -1,5 +1,230 @@
 package com.iispl.entity;
 
-public class InwardCheque {
+import java.math.BigDecimal;
+import java.time.LocalDate;
 
+import com.iispl.enums.InwardStatus;
+import com.iispl.enums.RepairStatus;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="inward_cheques")
+public class InwardCheque {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private String id;
+	
+	@Column(name="cheque_number")
+	private String chequeNumber;
+	
+	@Column(name="micr_code")
+	private String micrCode;
+	
+	@Column(name="original_micrcode")
+	private String originalMicrCode;
+	
+	@Column(name="amount")
+	private BigDecimal amount;
+	
+	@Column(name="drawerBank")
+	private String drawerBank;
+	
+	@Column(name="payeeName")
+	private String payeeName;
+	
+	@Column(name="accountNumber")
+	private String accountNumber;
+	
+	@Column(name="ifsc_code")
+	private String ifscCode;
+	
+	@Column(name="cheque_date")
+	private LocalDate chequeDate;
+	
+	@Column(name="dest_branch")
+	private String destBranch;
+	
+	@Column(name="inward_status")
+	private InwardStatus inwardStatus;
+	
+	@Column(name="repair_status")
+	private RepairStatus repairStatus;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "batch_id", nullable = true)
+	private Batch batch;
+	
+	
+	public InwardCheque() {}
+
+
+	public InwardCheque(String chequeNumber, String micrCode, String originalMicrCode,
+			BigDecimal amount, String drawerBank, String payeeName, String accountNumber, String ifscCode,
+			LocalDate chequeDate, String destBranch, InwardStatus inwardStatus, RepairStatus repairStatus) {
+		this.chequeNumber = chequeNumber;
+		this.micrCode = micrCode;
+		this.originalMicrCode = originalMicrCode;
+		this.amount = amount;
+		this.drawerBank = drawerBank;
+		this.payeeName = payeeName;
+		this.accountNumber = accountNumber;
+		this.ifscCode = ifscCode;
+		this.chequeDate = chequeDate;
+		this.destBranch = destBranch;
+		this.inwardStatus = inwardStatus;
+		this.repairStatus = repairStatus;
+	}
+
+
+	public String getId() {
+		return id;
+	}
+
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+
+	public String getChequeNumber() {
+		return chequeNumber;
+	}
+
+
+	public void setChequeNumber(String chequeNumber) {
+		this.chequeNumber = chequeNumber;
+	}
+
+
+	public String getMicrCode() {
+		return micrCode;
+	}
+
+
+	public void setMicrCode(String micrCode) {
+		this.micrCode = micrCode;
+	}
+
+
+	public String getOriginalMicrCode() {
+		return originalMicrCode;
+	}
+
+
+	public void setOriginalMicrCode(String originalMicrCode) {
+		this.originalMicrCode = originalMicrCode;
+	}
+
+
+	public BigDecimal getAmount() {
+		return amount;
+	}
+
+
+	public void setAmount(BigDecimal amount) {
+		this.amount = amount;
+	}
+
+
+	public String getDrawerBank() {
+		return drawerBank;
+	}
+
+
+	public void setDrawerBank(String drawerBank) {
+		this.drawerBank = drawerBank;
+	}
+
+
+	public String getPayeeName() {
+		return payeeName;
+	}
+
+
+	public void setPayeeName(String payeeName) {
+		this.payeeName = payeeName;
+	}
+
+
+	public String getAccountNumber() {
+		return accountNumber;
+	}
+
+
+	public void setAccountNumber(String accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+
+
+	public String getIfscCode() {
+		return ifscCode;
+	}
+
+
+	public void setIfscCode(String ifscCode) {
+		this.ifscCode = ifscCode;
+	}
+
+
+	public LocalDate getChequeDate() {
+		return chequeDate;
+	}
+
+
+	public void setChequeDate(LocalDate chequeDate) {
+		this.chequeDate = chequeDate;
+	}
+
+
+	public String getDestBranch() {
+		return destBranch;
+	}
+
+
+	public void setDestBranch(String destBranch) {
+		this.destBranch = destBranch;
+	}
+
+
+	public InwardStatus getInwardStatus() {
+		return inwardStatus;
+	}
+
+
+	public void setInwardStatus(InwardStatus inwardStatus) {
+		this.inwardStatus = inwardStatus;
+	}
+
+
+	public RepairStatus getRepairStatus() {
+		return repairStatus;
+	}
+
+
+	public void setRepairStatus(RepairStatus repairStatus) {
+		this.repairStatus = repairStatus;
+	}
+
+
+	public Batch getBatch() {
+		return batch;
+	}
+
+
+	public void setBatch(Batch batch) {
+		this.batch = batch;
+	}
+	
+	
+	
+	
 }
