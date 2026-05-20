38c6a2d200a03eb64dc8e07cb551b106347b40ad
sairamyasree-eng <sai.ramyasree@imageinfosystems.com>
2026-05-20 14:23:47 +0530

    added CtsSession,Batch entittes

--- src/main/java/com/iispl/entity/Batch.java
+++ src/main/java/com/iispl/entity/Batch.java
@@ -1,5 +1,161 @@
 package com.iispl.entity;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import com.iispl.enums.BatchStatus;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "batch")
 public class Batch {
+
+	@Id
+	@Column(name = "id", nullable = false, insertable = false, updatable = false)
+	private String id;
+
+	@Column(name = "batch_name", nullable = false)
+	private String batchName;
+
+	@Column(name = "session_id", nullable = false, insertable = false, updatable = false)
+	private String sessionId;
+
+	@Column(name = "created_by_user_id", nullable = false, insertable = false, updatable = false)
+	private String createdByUserId;
+
+	@Column(name = "batch_date", nullable = false)
+	private String batchDate;
+
+	@Column(name = "expected_cheque_count")
+	private int expectedChequeCount;
+
+	@Column(name = "expected_amount", precision = 15, scale = 2)
+	private BigDecimal expectedAmount;
+
+	@Enumerated(EnumType.STRING)
+	@Column(name = "Status", nullable = false)
+	private BatchStatus status;
+
+	@Column(name = "dem_file_path")
+	private String demFilePath;
+
+	@Column(name = "created_at")
+	private LocalDateTime createdAt;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "session_id", nullable = false)
+	private CtsSession session;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "session_id", nullable = false)
+	private User createdByUser;
+
+	public Batch() {
+	};
+
+	public Batch(String id, String batchName, String sessionId, String createdByUserId, String batchDate,
+			int expectedChequeCount, BigDecimal expectedAmount, BatchStatus status, String demFilePath,
+			LocalDateTime createdAt) {
+		super();
+		this.id = id;
+		this.batchName = batchName;
+		this.sessionId = sessionId;
+		this.createdByUserId = createdByUserId;
+		this.batchDate = batchDate;
+		this.expectedChequeCount = expectedChequeCount;
+		this.expectedAmount = expectedAmount;
+		this.status = status;
+		this.demFilePath = demFilePath;
+		this.createdAt = createdAt;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getBatchName() {
+		return batchName;
+	}
+
+	public void setBatchName(String batchName) {
+		this.batchName = batchName;
+	}
+
+	public String getSessionId() {
+		return sessionId;
+	}
+
+	public void setSessionId(String sessionId) {
+		this.sessionId = sessionId;
+	}
+
+	public String getCreatedByUserId() {
+		return createdByUserId;
+	}
+
+	public void setCreatedByUserId(String createdByUserId) {
+		this.createdByUserId = createdByUserId;
+	}
+
+	public String getBatchDate() {
+		return batchDate;
+	}
+
+	public void setBatchDate(String batchDate) {
+		this.batchDate = batchDate;
+	}
+
+	public int getExpectedChequeCount() {
+		return expectedChequeCount;
+	}
+
+	public void setExpectedChequeCount(int expectedChequeCount) {
+		this.expectedChequeCount = expectedChequeCount;
+	}
+
+	public BigDecimal getExpectedAmount() {
+		return expectedAmount;
+	}
+
+	public void setExpectedAmount(BigDecimal expectedAmount) {
+		this.expectedAmount = expectedAmount;
+	}
+
+	public BatchStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(BatchStatus status) {
+		this.status = status;
+	}
+
+	public String getDemFilePath() {
+		return demFilePath;
+	}
+
+	public void setDemFilePath(String demFilePath) {
+		this.demFilePath = demFilePath;
+	}
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
 
 }
--- src/main/java/com/iispl/entity/CtsSession.java
+++ src/main/java/com/iispl/entity/CtsSession.java
@@ -1,5 +1,131 @@
 package com.iispl.entity;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import com.iispl.enums.SessionStatus;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "cts_session")
+
 public class CtsSession {
+
+	@Id
+	@Column(name = "id", nullable = false, updatable = false)
+	private String id;
+
+	@Column(name = "opened_by_user_id", nullable = false, insertable = false, updatable = false)
+	private String openedByUserID;
+
+	@Enumerated(EnumType.STRING)
+	@Column(name = "status", nullable = false)
+	private SessionStatus status;
+
+	@Column(name = "session_date", nullable = false)
+	private LocalDate sessionDate;
+
+	@Column(name = "opened_at")
+	private LocalDateTime openedAt;
+
+	@Column(name = "closed_at")
+	private LocalDateTime closedAt;
+
+	@Column(name = "branch", nullable = false)
+	private String branch;
+
+	// hibernate relation MANY TO ONE RELATION
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "opened_by_user_id", nullable = false)
+	private User openedByUser;
+
+	// default constructor
+	public CtsSession() {
+	}
+
+	public CtsSession(String id, String openedByUserID, SessionStatus status, LocalDate sessionDate,
+			LocalDateTime openedAt, LocalDateTime closedAt, String branch) {
+		super();
+		this.id = id;
+		this.openedByUserID = openedByUserID;
+		this.status = status;
+		this.sessionDate = sessionDate;
+		this.openedAt = openedAt;
+		this.closedAt = closedAt;
+		this.branch = branch;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getOpenedByUserID() {
+		return openedByUserID;
+	}
+
+	public void setOpenedByUserID(String openedByUserID) {
+		this.openedByUserID = openedByUserID;
+	}
+
+	public SessionStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(SessionStatus status) {
+		this.status = status;
+	}
+
+	public LocalDate getSessionDate() {
+		return sessionDate;
+	}
+
+	public void setSessionDate(LocalDate sessionDate) {
+		this.sessionDate = sessionDate;
+	}
+
+	public LocalDateTime getOpenedAt() {
+		return openedAt;
+	}
+
+	public void setOpenedAt(LocalDateTime openedAt) {
+		this.openedAt = openedAt;
+	}
+
+	public LocalDateTime getClosedAt() {
+		return closedAt;
+	}
+
+	public void setClosedAt(LocalDateTime closedAt) {
+		this.closedAt = closedAt;
+	}
+
+	public String getBranch() {
+		return branch;
+	}
+
+	public void setBranch(String branch) {
+		this.branch = branch;
+	}
+
+	public User getOpenedByUser() {
+		return openedByUser;
+	}
+
+	public void setOpenedByUser(User openedByUser) {
+		this.openedByUser = openedByUser;
+	}
 
 }
